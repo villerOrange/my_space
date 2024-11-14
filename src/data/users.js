07894b8503ca6431a555e6f0ad0f654852181d8b
villerOrange <viller16@gmail.com>
2024-11-14 19:07:47 +0100

    Заменил картинку на ссылку для работы на pages

--- src/data/users.js
+++ src/data/users.js
@@ -4,7 +4,7 @@ const users = [
         fullName: 'Илья Болотских',
         quali: 'Студентос',
         about: 'Меня зовут Илья, я студент IT и фронтенд-разработчик, изучаю React и работаю над веб-проектами. Интересуюсь спортом, хорошей музыкой и кулинарией',
-        img: '/avatarIlia.jpg'
+        img: 'https://yt3.googleusercontent.com/r4CNhRD03iHyTEnpsIGCrVdoR5yww3MB1bcSznWN8TKt1lYfygLr3Is32sXRrIOpniCQRxac=s900-c-k-c0x00ffffff-no-rj'
     }
 ]
 export default users
